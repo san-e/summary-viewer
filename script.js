0800ee0e6813a9750c3c5c2514f8828a5080ff89
san-e <itzepyc@proton.me>
2025-12-16 10:13:31 +0100

    replace mathjax with katex

--- script.js
+++ script.js
@@ -177,7 +177,18 @@ function selectPost(title) {
       ${sectionsHtml}
     </div>
   `;
-  MathJax.typesetPromise();
+  renderMathInElement(document.body, {
+          // customised options
+          // • auto-render specific keys, e.g.:
+          delimiters: [
+              {left: '$$', right: '$$', display: true},
+              {left: '$', right: '$', display: false},
+              {left: '\\(', right: '\\)', display: false},
+              {left: '\\[', right: '\\]', display: true}
+          ],
+          // • rendering keys, e.g.:
+          throwOnError : false
+        });
   renderNav();
 }
 
